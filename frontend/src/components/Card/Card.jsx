151d3096ef6f7f761b453b008bbaae362d3978de
Shiwans Vaishya <shiwans.vaishya@gmail.com>
2024-09-01 18:38:47 +0530

    image problem solved

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import img from '../../assets/images/images.png'
 import './Card.css';
 
-const Card = ({ image, title, description }) => {
+const Card = ({ image,title, description }) => {
   return (
     <div className="card">
-      <img src={image} alt={title} className="card-image" />
+      <img src={image} alt={title} className='card-image'/>
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
