c82562969d72b8a5827c46e14fe6c42765f3236e
Dev Ghildiyal <ghildiyaldev1325@gmail.com>
2024-09-01 11:58:17 +0530

    Remove unnecessary line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-// import Home from './pages/Home';
-// import Events from './pages/Events';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <div className="content">
-        <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> */}
-        </Routes>
-      </div>
     </Router>
   );
 };
