4cfb90c1ee57491a0900508a7bb5a291300501f4
Dev Ghildiyal <ghildiyaldev1325@gmail.com>
2024-09-01 12:19:43 +0530

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <Home />
     </Router>
   );
 };
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Navbar from '../../components/Navbar/Navbar'
+import { Router } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <Router>
+        <Navbar/>
+        <Header/>
+    </Router>
+  )
+}
+
+export default Home
